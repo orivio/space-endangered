72256cd5a2a7e63c578b0bb684dd13c83ddf3676
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-03-06 13:02:48 -0500

    Obed - Restructuring TitleScreen for slick2d

--- src/main/java/com/obedotto/spaceendangered/game/TitleScreen.java
+++ src/main/java/com/obedotto/spaceendangered/game/TitleScreen.java
@@ -1,11 +1,26 @@
-package com.obedotto.spaceendangered.game; //should be "src.main.java.com.obedotto.spaceendangered.game"?
-
-public class TitleScreen implements Screen {
-
-
-
-}
-/*
-old:
 package com.obedotto.spaceendangered.game;
-*/
+
+public class TitleScreen implements BasicGameState {
+
+	public static final int ID = 0;
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int arg2) throws SlickException {
+		
+	}
+
+	@Override
+	public int getID() {
+		return TitleScreen.ID;
+	}
+}
