e68d7182fa2c9c6cfbf8b5a32a16da9873014d94
obedh <obedhenryotto@gmail.com>
2026-03-08 10:32:12 -0400

    Obed - Switched to EXTENDING BasicGameState instead of IMPLEMENTING GameState, which means we don't need to override all of those useless functions.

--- src/main/java/com/obedotto/spaceendangered/game/PlayScreen.java
+++ src/main/java/com/obedotto/spaceendangered/game/PlayScreen.java
@@ -6,7 +6,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.Color;
 
@@ -20,103 +20,13 @@ import org.newdawn.slick.TrueTypeFont;
 import java.awt.Font;
 
 
-public class PlayScreen implements GameState {
+public class PlayScreen extends BasicGameState {
 
     public static final int ID = 2;
 
     private List<AlienUnitCard> cards;
     private Image background;
     private Renderer renderer;
-
-    @Override
-    public void mouseClicked(int arg0, int arg1, int arg2, int arg3) {
-        
-    }
-
-    @Override
-    public void mouseDragged(int arg0, int arg1, int arg2, int arg3) {
-    }
-
-    @Override
-    public void mouseMoved(int arg0, int arg1, int arg2, int arg3) {
-    }
-
-    @Override
-    public void mousePressed(int arg0, int arg1, int arg2) {
-    }
-
-    @Override
-    public void mouseReleased(int arg0, int arg1, int arg2) {
-    }
-
-    @Override
-    public void mouseWheelMoved(int arg0) {
-    }
-
-    @Override
-    public void inputEnded() {
-    }
-
-    @Override
-    public void inputStarted() {
-    }
-
-    @Override
-    public boolean isAcceptingInput() {
-        return true;
-    }
-
-    @Override
-    public void setInput(Input arg0) {
-    }
-
-    @Override
-    public void keyPressed(int arg0, char arg1) {
-    }
-
-    @Override
-    public void keyReleased(int arg0, char arg1) {
-    }
-
-    @Override
-    public void controllerButtonPressed(int arg0, int arg1) {
-    }
-
-    @Override
-    public void controllerButtonReleased(int arg0, int arg1) {
-    }
-
-    @Override
-    public void controllerDownPressed(int arg0) {
-    }
-
-    @Override
-    public void controllerDownReleased(int arg0) {
-    }
-
-    @Override
-    public void controllerLeftPressed(int arg0) {
-    }
-
-    @Override
-    public void controllerLeftReleased(int arg0) {
-    }
-
-    @Override
-    public void controllerRightPressed(int arg0) {
-    }
-
-    @Override
-    public void controllerRightReleased(int arg0) {
-    }
-
-    @Override
-    public void controllerUpPressed(int arg0) {
-    }
-
-    @Override
-    public void controllerUpReleased(int arg0) {
-    }
 
     @Override
     public void enter(GameContainer arg0, StateBasedGame arg1) throws SlickException {
--- src/main/java/com/obedotto/spaceendangered/game/TitleScreen.java
+++ src/main/java/com/obedotto/spaceendangered/game/TitleScreen.java
@@ -5,108 +5,17 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
-import Game;
-
-public class TitleScreen implements GameState {
+public class TitleScreen extends BasicGameState {
 
 	public static final int ID = 0;
 	private Image titleScreenImage;
 	
-	@Override
-	public void mouseClicked(int arg0, int arg1, int arg2, int arg3) {
-	}
-
-	@Override
-	public void mouseDragged(int arg0, int arg1, int arg2, int arg3) {
-	}
-
-	@Override
-	public void mouseMoved(int arg0, int arg1, int arg2, int arg3) {
-	}
-
-	@Override
-	public void mousePressed(int arg0, int arg1, int arg2) {
-	}
-
-	@Override
-	public void mouseReleased(int arg0, int arg1, int arg2) {
-	}
-
-	@Override
-	public void mouseWheelMoved(int arg0) {
-	}
-
-	@Override
-	public void inputEnded() {
-	}
-
-	@Override
-	public void inputStarted() {
-	}
-
-	@Override
-	public boolean isAcceptingInput() {
-		return true;
-	}
-
-	@Override
-	public void setInput(Input arg0) {
-	}
-
-	@Override
-	public void keyPressed(int arg0, char arg1) {
-	}
-
-	@Override
-	public void keyReleased(int arg0, char arg1) {
-	}
-
-	@Override
-	public void controllerButtonPressed(int arg0, int arg1) {
-	}
-
-	@Override
-	public void controllerButtonReleased(int arg0, int arg1) {
-	}
-
-	@Override
-	public void controllerDownPressed(int arg0) {
-	}
-
-	@Override
-	public void controllerDownReleased(int arg0) {
-	}
-
-	@Override
-	public void controllerLeftPressed(int arg0) {
-	}
-
-	@Override
-	public void controllerLeftReleased(int arg0) {
-	}
-
-	@Override
-	public void controllerRightPressed(int arg0) {
-	}
-
-	@Override
-	public void controllerRightReleased(int arg0) {
-	}
-
-	@Override
-	public void controllerUpPressed(int arg0) {
-	}
-
-	@Override
-	public void controllerUpReleased(int arg0) {
-	}
-
 	@Override
 	public void enter(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 	}
