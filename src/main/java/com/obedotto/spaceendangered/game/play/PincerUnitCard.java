0c69c1d9a3438022ef595e48cdac91c8479ad752
obedh <obedhenryotto@gmail.com>
2026-03-09 05:51:23 -0400

    Obed - Fixed the card sprite filenames in ShooterUnitCard, NormalUnitCard, PincerUnitCard, ShieldUnitCard

--- src/main/java/com/obedotto/spaceendangered/game/play/PincerUnitCard.java
+++ src/main/java/com/obedotto/spaceendangered/game/play/PincerUnitCard.java
@@ -5,7 +5,7 @@ import com.obedotto.spaceendangered.assets.Sprite;
 
 public class PincerUnitCard extends AlienUnitCard {
     public PincerUnitCard() throws SlickException {
-        this.sprite = new Sprite("src/main/resources/sprites/alienPincer-card.png", 150, 150);
+        this.sprite = new Sprite("src/main/resources/sprites/alienPincer-card1.png", 150, 150);
         this.sprite.setX(15 + 175 + 175);
         this.sprite.setY(567);
     }
--- src/main/java/com/obedotto/spaceendangered/game/play/ShieldUnitCard.java
+++ src/main/java/com/obedotto/spaceendangered/game/play/ShieldUnitCard.java
@@ -5,7 +5,7 @@ import com.obedotto.spaceendangered.assets.Sprite;
 
 public class ShieldUnitCard extends AlienUnitCard {
     public ShieldUnitCard() throws SlickException {
-        this.sprite = new Sprite("src/main/resources/sprites/alienShield-card.png", 150, 150);
+        this.sprite = new Sprite("src/main/resources/sprites/alienShield-card1.png", 150, 150);
         this.sprite.setX(15 + 175 + 175 + 175);
         this.sprite.setY(567);
     }
