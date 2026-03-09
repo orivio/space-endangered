9b5cbeaa2adedd08bbbddde9b78b8a7faa1c2eae
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-03-09 13:18:50 -0400

    Obed - Added AlienUnit

---
+++ src/main/java/com/obedotto/spaceendangered/game/play/AlienUnit.java
@@ -0,0 +1,14 @@
+package com.obedotto.spaceendangered.game.play;
+
+public class AlienUnit {
+
+  private BattleCell container;
+  
+  public AlienUnit() {
+
+  }
+
+  public void setContainer(BattleCell container) {
+    this.container = container;
+  }
+}
