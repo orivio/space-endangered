bab3c3579cc0a4d0aca5e4ff65513bf99fc5e9d7
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-03-09 13:08:10 -0400

    Obed - Initializing a grid of battle cells

--- src/main/java/com/obedotto/spaceendangered/game/play/BattleField.java
+++ src/main/java/com/obedotto/spaceendangered/game/play/BattleField.java
@@ -2,4 +2,33 @@ package com.obedotto.spaceendangered.game.play;
 
 public class BattleField {
 
+  private BattleCell[][] grid;
+
+  public BattleField() {
+    this.grid = new BattleCell[5][6];
+    for(int i = 0; i < 5; i ++) {
+      for(int j = 0; j < 6; j ++) {
+        if(j > 0) {
+          this.grid[i][j].north = this.grid[i][j-1];
+        }
+        if(j < 5) {
+          this.grid[i][j].south = this.grid[i][j+1];
+        }
+        
+        if(i > 0) {
+          this.grid[i][j].north = this.grid[i-1][j];
+        }
+        if(i < 4) {
+          this.grid[i][j].south = this.grid[i+1][j];
+        }
+      }
+    }
+  }
+  
+  public void update(float deltaTime) {
+    for(BattleCell cell : grid) {
+      cell.update(deltaTime);
+    }
+  }
+  
 }
